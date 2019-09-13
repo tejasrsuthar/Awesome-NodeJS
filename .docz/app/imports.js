0a319962dfd29c69971431f06504f844bf9d965a
Tejas Suthar <tsuthar@mobiquityinc.com>
2019-09-13 13:42:35 +0530

    make separate resources section as a part of docz page

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -3,4 +3,12 @@ export const imports = {
     import(
       /* webpackPrefetch: true, webpackChunkName: "readme" */ 'README.mdx'
     ),
+  'articles-tutorials-blog.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "articles-tutorials-blog" */ 'articles-tutorials-blog.mdx'
+    ),
+  'node-js-community.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "node-js-community" */ 'node-js-community.mdx'
+    ),
 }
